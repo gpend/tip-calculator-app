4f7979639ca5c17f8b964f2db1f620deb54bd4ec
Greg Pendleton <gpendleton@gmail.com>
2021-10-10 16:26:44 -0500

    refactored script to make it more responsive

--- script.js
+++ script.js
@@ -10,19 +10,30 @@ const percent20Button = document.querySelector('.btn-20')
 const perPersonTipP = document.getElementById('per-person-tip-total')
 const perPersonTotalP = document.getElementById('per-person-total-amount')
 let tipAmount = 0
+let peopleNum = 1
+let billAmount = 0
 
-function TipChange(tip){
-    let tipPercent = tip * .01
-    tipAmount = eval(billAmountInput.value * tipPercent)
-    return tipAmount
-}
-
-function PeopleAdded (peopleNum){
+function CalculateTip(){
     let tipPerPerson = tipAmount / peopleNum;
     let totalPerPerson = (eval(billAmountInput.value) + tipPerPerson) / peopleNum;
     perPersonTipP.innerText = `$${tipPerPerson}`
     perPersonTotalP.innerText = `$${totalPerPerson}`
-    return tipPerPerson, totalPerPerson 
+}
+
+function TipChange(tip){
+    let tipPercent = tip * .01
+    tipAmount = eval(billAmountInput.value * tipPercent)
+    CalculateTip()
+}
+
+function BillAmountAdded(billAmountNum){
+    billAmount = billAmountNum
+    CalculateTip()
+}
+
+function PeopleAdded (peopleNumInput){
+    peopleNum = peopleNumInput
+    CalculateTip()
 }
 
 percent5Button.addEventListener('click', function() {TipChange(5)})
@@ -30,7 +41,11 @@ percent10Button.addEventListener('click', function() {TipChange(10)})
 percent15Button.addEventListener('click', function() {TipChange(15)})
 percent25Button.addEventListener('click', function() {TipChange(25)})
 percent20Button.addEventListener('click', function() {TipChange(20)})
+customPercentInput.addEventListener('keyup', function(){TipChange(customPercentInput.value)})
 peopleAmountInput.addEventListener('keyup', function(){ PeopleAdded(peopleAmountInput.value)})
+billAmountInput.addEventListener('keyup', function() {BillAmountAdded(billAmountInput.value)})
 
 // recalculate on any input
 // defaults for basic calculations
+
+CalculateTip()

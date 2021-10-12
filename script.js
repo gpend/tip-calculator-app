0b2146fe13c1d8dafc47f44afaa48f6aae5152aa
Greg Pendleton <gpendleton@gmail.com>
2021-10-12 10:19:16 -0500

    enabled reset button

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ const percent10Button = document.querySelector('.btn-10')
 const percent15Button = document.querySelector('.btn-15')
 const percent25Button = document.querySelector('.btn-25')
 const percent20Button = document.querySelector('.btn-20')
+const resetButton = document.querySelector('.btn-reset')
 const perPersonTipP = document.getElementById('per-person-tip-total')
 const perPersonTotalP = document.getElementById('per-person-total-amount')
+
+
 let tipPercent = 20
 let peopleNum = 1
 let billAmount = 0
@@ -45,7 +48,7 @@ function PeopleAdded (peopleNumInput){
     CalculateTip()
 }
 
-percent5Button.addEventListener('click', function() {tipPercent = 5; CalculateTip()}) // TODO change to just return?? tip = 5?
+percent5Button.addEventListener('click', function() {tipPercent = 5; CalculateTip()})
 percent10Button.addEventListener('click', function() {tipPercent = 10; CalculateTip()})
 percent15Button.addEventListener('click', function() {tipPercent = 15; CalculateTip()})
 percent25Button.addEventListener('click', function() {tipPercent = 25; CalculateTip()})
@@ -53,8 +56,15 @@ percent20Button.addEventListener('click', function() {tipPercent = 20; Calculate
 customPercentInput.addEventListener('keyup', function(){tipPercent = (customPercentInput.value); CalculateTip()})
 peopleAmountInput.addEventListener('keyup', function(){ PeopleAdded(peopleAmountInput.value)})
 billAmountInput.addEventListener('keyup', function() {BillAmountAdded(billAmountInput.value)})
+resetButton.addEventListener('click', function() {  tipPercent = 20;
+                                                    billAmountInput.value = ""
+                                                    billAmount = 0; 
+                                                    peopleAmountInput.value = ""
+                                                    peopleNum = 1; 
+                                                    CalculateTip()
+                                                }
+                            )
 
-// TODO enable reset
 // TODO animate buttons
 // TODO enable big screens
 
